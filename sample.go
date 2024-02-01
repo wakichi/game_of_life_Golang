69bf9a551624f374d0d96018f56449ead62d9c6e
Kohei Wakita <k.wakita20@gmail.com>
2024-02-01 19:20:31 +0900

    input function

---
+++ sample.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var width, height int
+	fmt.Println("enter width as int")
+	fmt.Scan(&width)
+	fmt.Println(width)
+	fmt.Println("enter height as int")
+	fmt.Scan(&height)
+	fmt.Println(height)
+
+	
+}
