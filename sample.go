2c47ea41f5d9a4dcd8c68a5eb369e18e2bff855e
Kohei Wakita <k.wakita20@gmail.com>
2024-02-05 15:21:16 +0900

    implement countLiveNeighbor but dont work

--- sample.go
+++ sample.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 )
+
 func printList(list [][]int){
 	for i:=0; i<len(list); i++{
 		fmt.Println(list[i])
@@ -30,25 +31,68 @@ func main(){
 		}
 	}
 	printList(life)
+	fmt.Println("")
 
 	// turn
 	lifeNext := make([][]int, height)
+	lifeCount:=make([][]int, height)
+	for i := 0; i < height; i++{
+		lifeCount[i] = make([]int, width)
+	}
 	copy(lifeNext, life)
-	for i:=0; i<width; i++{
-		for j:=0; j<width; j++{
-			liveNeighbor := countLiveNeighbor(life, i, j)
+	printList(life)
+	fmt.Println("")
+	for y:=0; y<height; y++{
+		for x:=0; x<width; x++{
+			liveNeighbor := countLiveNeighbor(life, x, y, width, height)
+			// for debug
+			lifeCount[y][x] = liveNeighbor
 			//lifeNextのijを更新する
 			//lifeNextのijのポインタ、liveNeighborを渡せば良さそう？
-			updateOneCell(&life[i][j], liveNeighbor)
+			updateOneCell(&lifeNext[y][x],life[y][x], liveNeighbor)
 		}
 	}
-	life = lifeNext
+	printList(lifeCount)
+	fmt.Println("")
 	printList(lifeNext)
 }
 
-func countLiveNeighbor(list [][]int, x int, y int)int{
-	return 0
+func countLiveNeighbor(list [][]int, x int, y int, width int, height int) int {
+	count :=0
+
+	//遷移のパターンを全列挙、枠外は除外
+	for i := -1; i<2; i++{
+		for j := -1; j<2; j++{
+			if i ==0 && j ==0 {
+				continue
+			}
+			//debug nx, nyの列挙は良さそう
+			nx:=x+j
+			ny:=y+i
+			//ngパターンを除外
+			if !(nx<width && nx>=0 && ny<height && ny>=0) {
+				continue
+			}
+			count+=list[ny][nx]
+			if x == 3 && y == 3{
+
+				fmt.Println(nx, ny, list[ny][nx],count)
+			}
+		}
+	}
+	return count
 }
-func updateOneCell(cell *int, liveNeighbor int){
+
+func updateOneCell(nextCell *int, nowCell int, liveNeighbor int){
+	//birth rule
+	if nowCell == 0 && liveNeighbor == 3{
+		*nextCell = 1
 	
+	}else if nowCell == 1 && (liveNeighbor ==2 ||liveNeighbor ==3){
+	//live rule
+		*nextCell = 1
+	}else{
+		// death rule
+		*nextCell = 0
+	}
 }
